558aefe8d7f5102df822084426792a5dae046d1e
Mavi <mariapimenta2607@gmail.com>
2024-08-02 10:36:26 -0300

    finish

--- aulaUm/src/wind/Wind.jsx
+++ aulaUm/src/wind/Wind.jsx
@@ -54,7 +54,7 @@ export const Wind = () => {
                         <div className="flex items-center justify-center absolute w-full">
                             <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative flex flex-col" role="alert">
                                 <strong className="font-bold">Sucesso!</strong>
-                                <span className="block">Sua ação foi realizada com sucesso.</span>
+                                <span className="block">Você clicou no botão.</span>
                             </div>
                         </div>
                     </>
